a945cb8afb8538e0eac33d756c0842f64fb9bca2
Harsh Dhaduk <harshdhaduk@ip-10-10-104-43.ap-southeast-1.compute.internal>
2020-09-09 09:28:16 +0530

    [UPD] Tutorial Upgrade

--- custom_addons/school_student/models/models.py
+++ custom_addons/school_student/models/models.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import datetime
 
 from odoo import models, fields, api, _
 from odoo.exceptions import  UserError
@@ -7,7 +8,7 @@ class school_student(models.Model):
     _name = 'school.student'
     _description = 'school_student.school_student'
 
-    name = fields.Char()
+    name = fields.Char(default="Sunny Leone")
     school_id = fields.Many2one("school.profile", string="School Name")
     hobby_list = fields.Many2many("hobby", "school_hobby_rel","student_id",
                                   "hobby_id", string="Hobby List",
@@ -25,13 +26,84 @@ class school_student(models.Model):
                                ('account.move', 'Invoice')] ,
                               string="Reference Field",
                               default="school.profile,1")
+    active = fields.Boolean(string="Active", default=True)
+    bdate = fields.Date(string="Date Of Birth", required=True)
+    student_age = fields.Char(string="Total Age", compute="_get_age_from_student")
+
+    @api.depends("bdate")
+    def _get_age_from_student(self):
+        """Age Calculation"""
+        today_date = datetime.date.today()
+        for stud in self:
+            if stud.bdate:
+                """
+                Get only year.
+                """
+                # bdate = fields.Datetime.to_datetime(stud.bdate).date()
+                # total_age = str(int((today_date - bdate).days / 365))
+                # stud.student_age = total_age
+
+                """
+                Origin of below source code
+                https://gist.github.com/shahri23/1804a3acb7ffb58a1ec8f1eda304af1a
+                """
+                currentDate = datetime.date.today()
+
+                deadlineDate= fields.Datetime.to_datetime(stud.bdate).date()
+                print (deadlineDate)
+                daysLeft = currentDate - deadlineDate
+                print(daysLeft)
+
+                years = ((daysLeft.total_seconds())/(365.242*24*3600))
+                yearsInt=int(years)
+
+                months=(years-yearsInt)*12
+                monthsInt=int(months)
+
+                days=(months-monthsInt)*(365.242/12)
+                daysInt=int(days)
+
+                hours = (days-daysInt)*24
+                hoursInt=int(hours)
+
+                minutes = (hours-hoursInt)*60
+                minutesInt=int(minutes)
+
+                seconds = (minutes-minutesInt)*60
+                secondsInt =int(seconds)
+
+                stud.student_age = 'You are {0:d} years, {1:d}  months, {2:d}  days, {3:d}  hours, {4:d} \
+                 minutes, {5:d} seconds old.'.format(yearsInt,monthsInt,daysInt,hoursInt,minutesInt,secondsInt)
+            else:
+                stud.student_age = "Not Providated...."
 
 
-    def write(self, values):
-        rtn = super(school_student, self).write(values)
-        if not self.hobby_list:
-            raise UserError(_("Please select at least one hobby."))
-        return rtn
+
+    # @api.model_create_multi
+    # def create(self, values):
+    #     rtn = super(school_student, self).create(values)
+    #     return rtn
+
+    # @api.model
+    # def create(self, values):
+    #     rtn = super(school_student, self).create(values)
+    #     return rtn
+
+    #No Decorator
+    # def write(self, values):
+    #     rtn = super(school_student, self).write(values)
+    #     return rtn
+
+    # @api.returns('self', lambda value: value.id)
+    # def copy(self, default = {}):
+    #     #default['active'] = False
+    #     default['name'] = "copy ("+self.name+")"
+    #     rtn = super(school_student, self).copy(default=default)
+    #     rtn.total_fees = 500
+    #     return rtn
+
+
+
 
 class SchoolProfile(models.Model):
     _inherit = "school.profile"
