cefe9665b96cd38c850d63f33512eb5fc839eebb
Harsh Dhaduk <eng.cmp.harsh@gmail.com>
2021-09-02 12:14:09 +0530

    Last session code upgrade.

--- custom_addons/school_student/models/models.py
+++ custom_addons/school_student/models/models.py
@@ -85,6 +85,10 @@ class school_student(models.Model):
         ('total_fees_check', 'check(total_fees>100)', 'minimum 101 amount allow.')
     ]
 
+    def buttonClickEvent(self):
+
+        raise UserError(_("You click this button. Woohooo!"))
+
     def specialCommand6(self):
         ids = [15, 31, 32]
         self.write({"hobby_list": [(6, 0, ids)]})
